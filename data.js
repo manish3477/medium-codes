30d7de3176b7cffe6ff55386d0f613cbb19f3b79
Manish Parajuli <83599079+manish3477@users.noreply.github.com>
2025-02-09 01:03:42 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,74 @@
+  items: [
+    {
+      id: 1,
+      name: "oven",
+      quantity: "22",
+      rate: "1200",
+      discountList: [
+        {
+          id: 1,
+          name: "festive discount",
+          rate: "25",
+          type: "percent",
+        },
+        {
+          id: 1,
+          name: "new year discount",
+          rate: "50",
+          type: "flat",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "rice cooker",
+      quantity: "12",
+      rate: "200",
+    },
+    {
+      id: 3,
+      name: "smoothy blender",
+      quantity: "6",
+      rate: "180",
+    },
+    {
+      id: 4,
+      name: "toaster",
+      quantity: "4",
+      rate: "120",
+    },
+  ],
+
+  discounts: [
+    {
+      id: 1,
+      name: "festival discount",
+      type: "percentage",
+      rate: 30,
+    },
+    {
+      id: 2,
+      name: "lucky discount",
+      type: "flat",
+      rate: 60,
+    },
+    {
+      id: 3,
+      name: "opening discount",
+      type: "percentage",
+      rate: 10,
+    },
+  ],
+
+  taxes: [
+    {
+      id: 1,
+      name: "GST",
+      rate: 13,
+    },
+    {
+      id: 1,
+      name: "Custom duty",
+      rate: 25,
+    },
+  ],
